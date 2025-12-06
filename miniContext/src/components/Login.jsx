aa7ddeb1a75ea3534d930897525c40a4fa5f58de
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2025-12-06 23:35:19 +0530

    Learn useContext

---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setpassword] = useState("");
+  const {setUser} = useContext(UserContext) 
+
+  const handleSubmit = (e) => {
+   e.preventDefault()
+   setUser({username,password})
+  };
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        placeholder="username"
+      />
+      {" "}
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => {
+          setpassword(e.target.value);
+        }}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default Login;
